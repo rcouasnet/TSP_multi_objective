1fd3a5ba7b72b9290c19446f23df5d9b8c767a7f
flodavid <fl.david.53@gmail.com>
2016-03-27 22:38:00 +0200

    ajout des fichiers de la classe Evaluation oubliés

---
+++ evaluation.cpp
@@ -0,0 +1,6 @@
+#include "evaluation.h"
+
+bool Evaluation::is_dominated(const Evaluation& other) const
+{
+    return (other.get_val1() < val_obj1 && other.get_val2() < val_obj2);
+}
---
+++ evaluation.h
@@ -0,0 +1,27 @@
+#ifndef EVALUATION_H
+#define EVALUATION_H
+
+class Evaluation
+{
+private:
+    double val_obj1, val_obj2;
+    
+public:
+    Evaluation(double _val_obj1, double _val_obj2) :
+	val_obj1(_val_obj1), val_obj2(_val_obj2) {};
+	
+    double get_val1() const { return val_obj1 ;}
+    double get_val2() const { return val_obj2 ;}
+    
+    /**
+     * Regarde si l'évaluation est dominée par une autre
+     * @param other : l'autre évaluation
+     * @return true si la solution est dominée (en minimisation)
+     */
+    bool is_dominated(const Evaluation& other) const;
+    
+    void set_obj1(double cost) { val_obj1= cost; }
+    void set_obj2(double cost) { val_obj2= cost; }
+};
+
+#endif // EVALUATION_H
